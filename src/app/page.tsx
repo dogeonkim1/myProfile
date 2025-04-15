da9b75f051ce17129d02dfc6ad8a81b0ce60d322
dogeonkim1 <caddibo4@naver.com>
2025-04-15 20:02:16 +0900

    layout 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import FadeInSection from "@/components/FadeInSection";
 import Hero from "@/components/Hero";
 import SocialSidebar from "@/components/SocialSidebar";
+import SkillsSection from "@/components/SkillsSection";
 
 export default function Home() {
   return (
-    <main className="px-4 max-w-[1200px] mx-auto relative">
+    <main className="px-4 max-w-[1000px] mx-auto relative">
         <SocialSidebar />
-      <Hero/>
+        {/*</Hero>*/}
+        <Hero/>
           {/*이 후 내용들 예:ImageSlideIn 컴포넌트들*/}
-      {/*</Hero>*/}
+        <FadeInSection>
+            <SkillsSection />
+        </FadeInSection>
         <FadeInSection>
             <p className="text-xl mt-6 leading-relaxed">
                 동찬아 훈련 잘 받고 혹시 불침번도 서니?? 진짜 극혐이다~
--- src/components/SocialSidebar.tsx
+++ src/components/SocialSidebar.tsx
@@ -4,7 +4,7 @@ import {FaInstagram, FaPhoneAlt, FaGithub} from 'react-icons/fa'
 
 export default function SocialSidebar() {
     return(
-        <div className="absolute top-10 left-5 flex flex-col items-center gap-6 z-50">
+        <div className="fixed top-10 left-5 flex flex-col items-center gap-6 z-50">
             <a href="https://www.instagram.com/dogeonni/" target="_blank" rel="noopener noreferrer">
                 <FaInstagram className="text-xl text-gray-700 hover:text-pink-500 transition" />
             </a>
@@ -14,7 +14,7 @@ export default function SocialSidebar() {
             <a href="https://github.com/dogeonkim1" target="_blank" rel="noopener noreferrer">
                 <FaGithub className="text-xl text-gray-700 hover:text-black transition" />
             </a>
-            <div className="w-px h-24 bg-gray-400 mt-2"/>
+            {/*<div className="w-px h-24 bg-gray-400 mt-2"/>*/}
         </div>
     )
 }
