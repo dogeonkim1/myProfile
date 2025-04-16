edf9667a4b4d07ecb6586594603a76cf6e310708
dogeonkim1 <caddibo4@naver.com>
2025-04-16 17:32:31 +0900

    add CardSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,56 +2,21 @@ import FadeInSection from "@/components/FadeInSection";
 import Hero from "@/components/Hero";
 import SocialSidebar from "@/components/SocialSidebar";
 import SkillsSection from "@/components/SkillsSection";
+import CardSection from "@/components/CardSection";
 
 export default function Home() {
   return (
     <main className="px-4 max-w-[1000px] mx-auto relative">
         <SocialSidebar />
-        {/*</Hero>*/}
         <Hero/>
           {/*이 후 내용들 예:ImageSlideIn 컴포넌트들*/}
-        <FadeInSection className="mt-24">
-            <SkillsSection />
+        <FadeInSection>
+            <div className="mb-0">
+                <SkillsSection />
+            </div>
         </FadeInSection>
         <FadeInSection>
-            <p className="text-xl mt-6 leading-relaxed ">
-                Ca c'est test
-            </p>
-        </FadeInSection>
-        <FadeInSection>
-            <p className="text-xl mt-6 leading-relaxed">
-                Ca c'est test
-            </p>
-        </FadeInSection>
-        <FadeInSection>
-            <p className="text-xl mt-6 leading-relaxed">
-                Ca c'est test
-            </p>
-        </FadeInSection>
-        <FadeInSection>
-            <p className="text-xl mt-6 leading-relaxed">
-                Ca c'est test
-            </p>
-        </FadeInSection>
-        <FadeInSection>
-            <p className="text-xl mt-6 leading-relaxed">
-                Ca c'est test
-            </p>
-        </FadeInSection>
-        <FadeInSection>
-            <p className="text-xl mt-6 leading-relaxed">
-                Ca c'est test~
-            </p>
-        </FadeInSection>
-        <FadeInSection>
-            <p className="text-xl mt-6 leading-relaxed">
-                Ca c'est test
-            </p>
-        </FadeInSection>
-        <FadeInSection>
-            <p className="text-xl mt-6 leading-relaxed">
-                Ca c'est test
-            </p>
+            <CardSection />
         </FadeInSection>
 
     </main>
