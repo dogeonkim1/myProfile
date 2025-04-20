69cca38d1f2a9df24a2953d23061a2d7c369efc1
dogeonkim1 <caddibo4@naver.com>
2025-04-20 21:58:57 +0900

    3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
+// import type { NextConfig } from "next";
+//
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+//
+// export default nextConfig;
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    basePath: isProd ? "/myProfile" : "",
+    assetPrefix: isProd ? "/myProfile/" : "",
 };
 
 export default nextConfig;
