02520adc7d64db141fd260d72b279aa853bf7644
dogeonkim1 <caddibo4@naver.com>
2025-04-23 18:21:10 +0900

    fixed: ready to vercel

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,14 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-    output: "export",
-    basePath: isProd ? "/myProfile" : "",
-    assetPrefix: isProd ? "/myProfile/" : "",
+    // output: "export",
+    basePath: isProd ? "" : "",
+    assetPrefix: isProd ? "" : "",
     images: {
         unoptimized: true,
     },
     eslint: {
-        ignoreDuringBuilds: true,  // Vercel 빌드 시 ESLint 오류 무시
+        ignoreDuringBuilds: true, // Vercel 빌드 시 ESLint 오류 무시
     },
 };
 
